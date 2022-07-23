468544357bd814f596eed9e75a6f7692abdd8234
NiceLeader <maciek2002.10@wp.pl>
2022-07-23 12:18:41 +0200

    I changed the language to English

--- src/Mastermind.java
+++ src/Mastermind.java
@@ -3,16 +3,15 @@ import java.util.InputMismatchException;
 import java.util.Random;
 import java.util.Scanner;
 
-// Autor: Maciej Lewandowski
-//        Gracz ma 10 szans na odgadniêcie kodu.
-//        Po ka¿dej próbie odgadniêcia kodu  komputer ocenia
-//        – ile cyfr w kodzie jest na swoim miejscu: punktacja 1
-//        -  ile cyfr jest w kodzie ale nie na swoim miejscu: punktacja 0
-//        je¿eli podana cyfra nie wystêpuje w kodzie nie dostaje ¿adnego punktu
-//        punktacja jest podawana tak, ¿eby nie zdradziæ do których miejsc w kodzie siê odnosi
-//        najpierw podawane s¹ pe³ne punkty a potem zera
+// Author: Maciej Lewandowski
+//        The player has 10 chances to guess the code.
+//        After each attempt to guess the code, the computer evaluates it.
+//        - how many digits in the code are in place: score 1
+//        if the given digit is not present in the code you get no point
+//        the score is given so as not to reveal which places in the code it refers to
+//        full points are given first, then zeros
 
-// todo: ³apanie b³êdu InputMismatchException
+// todo: InputMismatchException
 public class Mastermind {
 
     public static void main(String[] args) {
@@ -33,25 +32,25 @@ public class Mastermind {
         ArrayList<String> result = new ArrayList<>();
         boolean winner = false;
         
-        System.out.println("Witaj w Mastermind!");
+        System.out.println("Welcome to Mastermind!");
 
         while (counter < 10) {
             counter++;
             int position = 1;
 
 
-            System.out.println(" Próba nr " + counter);
+            System.out.println(" Attempt number " + counter);
 
             for (int i = 0; i < 4; i++) {
-                System.out.println("Miejsce " + position++ + "");
+                System.out.println("Position " + position++ + "");
                 guess[i] = sc.nextInt();
 
                 try {
                     if (guess[i] > 6 || guess[i] < 1) {
-                        throw new IllegalArgumentException("Strzelaj pomiêdzy 1 a 6!");
+                        throw new IllegalArgumentException("Choose between 1 and 6!");
                     }
                 } catch (IllegalArgumentException e) {
-                    System.out.println("Strzelaj pomiêdzy 1 a 6!");
+                    System.out.println("Choose between 1 a 6!");
                 }
             }
             for (int i = 0; i < 4; i++) {
@@ -81,20 +80,20 @@ public class Mastermind {
                 break;
             }
 
-            System.out.print(" Twoja punktacja wygl¹da nastêpuj¹co: " + result);
+            System.out.print(" Your score is as follows:   " + result);
 
             result.clear();
 
             if (counter == 10) {
-                System.out.println(" Niestety nie uda³o Ci siê odgadn¹æ mojego kodu. ");
-                System.out.println(" Mój kod to: ");
+                System.out.println(" Unfortunately you didn't guess my code. ");
+                System.out.println(" Code: ");
                 break;
             }
 
         }
 
         if (winner) {
-            System.out.println("Brawo, wygra³eœ!");
+            System.out.println("Congrats, you won!");
         }
         for (int i = 0; i < 4; i++) {
             System.out.print(thread[i] + " ");
